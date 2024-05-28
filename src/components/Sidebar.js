1b7eb993c0290cf3b16ed5d0bc4cc17e2d8abea2
epemba1 <eric-younous.pemba1@univ-lyon2.fr>
2024-05-28 08:54:25 +0200

    Add style sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,21 +2,26 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { List, ListItem, ListItemIcon, ListItemText, styled } from '@mui/material';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeIcon from '@mui/icons-material/Home';
 import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined'; 
+import WorkIcon from '@mui/icons-material/Work';
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
 import SummaryOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
+import SummaryIcon from '@mui/icons-material/Summarize';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import HelpIcon from '@mui/icons-material/Help';
 
 const menuItems = [
-  { text: 'Accueil', icon: <HomeOutlinedIcon />, path: '/' },
-  { text: "Sélection de l'activité", icon: <WorkOutlineOutlinedIcon />, path: '/selection-activite' },
-  { text: "Localisation d'implantation", icon: <FmdGoodOutlinedIcon />, path: '/localisation-implantation' },
-  { text: 'Synthèse de la recherche', icon: <SummaryOutlinedIcon />, path: '/synthese-recherche' },
-  { text: 'Aide', icon: <HelpOutlineOutlinedIcon />, path: '/aide' },
+  { text: 'Accueil', icon: <HomeOutlinedIcon />, activeIcon: <HomeIcon />, path: '/' },
+  { text: "Sélection de l'activité", icon: <WorkOutlineOutlinedIcon />, activeIcon: <WorkIcon />, path: '/selection-activite' },
+  { text: "Localisation d'implantation", icon: <FmdGoodOutlinedIcon />, activeIcon: <FmdGoodIcon />, path: '/localisation-implantation' },
+  { text: 'Synthèse de la recherche', icon: <SummaryOutlinedIcon />, activeIcon: <SummaryIcon />, path: '/synthese-recherche' },
+  { text: 'Aide', icon: <HelpOutlineOutlinedIcon />, activeIcon: <HelpIcon />, path: '/aide' },
 ];
 
 const SidebarContainer = styled('div')(({ theme, sidebarOpen }) => ({
-  width: sidebarOpen ? 280 : 64, // Increase width to accommodate longer text
+  width: sidebarOpen ? 280 : 64,
   transition: 'width 0.3s',
   marginTop: theme.spacing(8),
   position: 'fixed',
@@ -28,7 +33,7 @@ const SidebarContainer = styled('div')(({ theme, sidebarOpen }) => ({
 }));
 
 const StyledListItemIcon = styled(ListItemIcon)({
-  minWidth: 36, // Adjust the minimum width to create appropriate space between icon and label
+  minWidth: 36,
 });
 
 const Sidebar = ({ sidebarOpen }) => {
@@ -37,25 +42,34 @@ const Sidebar = ({ sidebarOpen }) => {
   return (
     <SidebarContainer sidebarOpen={sidebarOpen}>
       <List>
-        {menuItems.map((item) => (
-          <ListItem
-            button
-            component={Link}
-            to={item.path}
-            key={item.text}
-            style={{ justifyContent: sidebarOpen ? 'initial' : 'center' }}
-          >
-            <StyledListItemIcon style={{ color: location.pathname === item.path ? '#286AC7' : '#000' }}>
-              {item.icon}
-            </StyledListItemIcon>
-            {sidebarOpen && (
-              <ListItemText
-                primary={item.text}
-                style={{ color: location.pathname === item.path ? '#286AC7' : '#000'}}
-              />
-            )}
-          </ListItem>
-        ))}
+        {menuItems.map((item) => {
+          const isActive = location.pathname === item.path;
+          return (
+            <ListItem
+              button
+              component={Link}
+              to={item.path}
+              key={item.text}
+              style={{ 
+                justifyContent: sidebarOpen ? 'initial' : 'center',
+                backgroundColor: isActive ? '#f5f5f5' : 'transparent'
+              }}
+            >
+              <StyledListItemIcon style={{ color: isActive ? '#286AC7' : '#000' }}>
+                {isActive ? item.activeIcon : item.icon}
+              </StyledListItemIcon>
+              {sidebarOpen && (
+                <ListItemText
+                  primary={item.text}
+                  style={{ 
+                    color: isActive ? '#286AC7' : '#000', 
+                    fontWeight: isActive ? 'bold' : 'normal' 
+                  }}
+                />
+              )}
+            </ListItem>
+          );
+        })}
       </List>
     </SidebarContainer>
   );
