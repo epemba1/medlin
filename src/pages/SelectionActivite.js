afc72ca1a66f22cf35b333b0820ab4dd07feda03
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-05-30 22:16:18 +0200

    Update SelectionActivite.js

--- src/pages/SelectionActivite.js
+++ src/pages/SelectionActivite.js
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Box, Grid, FormControl, Skeleton, Snackbar, Chip, Button, IconButton, Alert, AlertTitle } from '@mui/material';
+import { Container, Typography, Box, Grid, FormControl, Skeleton, Snackbar, Button, IconButton, Alert, AlertTitle, Paper } from '@mui/material';
 import Select from 'react-select';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 const SelectionActivite = () => {
   const [data, setData] = useState(null);
-  const [selectedSection, setSelectedSection] = useState(null);
-  const [selectedDivision, setSelectedDivision] = useState(null);
-  const [selectedGroup, setSelectedGroup] = useState(null);
-  const [selectedClasses, setSelectedClasses] = useState([]);
-  const [selectedSubclass, setSelectedSubclass] = useState(null);
+  const [selectedSection, setSelectedSection] = useState(localStorage.getItem('selectedSection') ? JSON.parse(localStorage.getItem('selectedSection')) : null);
+  const [selectedDivision, setSelectedDivision] = useState(localStorage.getItem('selectedDivision') ? JSON.parse(localStorage.getItem('selectedDivision')) : null);
+  const [selectedGroup, setSelectedGroup] = useState(localStorage.getItem('selectedGroup') ? JSON.parse(localStorage.getItem('selectedGroup')) : null);
+  const [selectedClass, setSelectedClass] = useState(localStorage.getItem('selectedClass') ? JSON.parse(localStorage.getItem('selectedClass')) : null);
+  const [selectedSubclass, setSelectedSubclass] = useState(localStorage.getItem('selectedSubclass') ? JSON.parse(localStorage.getItem('selectedSubclass')) : null);
   const [loading, setLoading] = useState(true);
-  const [selectedActivities, setSelectedActivities] = useState([]);
   const [alertMessage, setAlertMessage] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [firstVisit, setFirstVisit] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -26,96 +26,64 @@ const SelectionActivite = () => {
         .then(response => {
           setData(response.data);
           setLoading(false);
+          setFirstVisit(false);
         })
-        .catch(error => console.error('Error loading data:', error));
+        .catch(error => {
+          setLoading(false);
+          setAlertMessage('Erreur de chargement des données');
+          setOpenSnackbar(true);
+        });
     }, 2000);
-
-    const savedSelectedSection = localStorage.getItem('selectedSection');
-    const savedSelectedDivision = localStorage.getItem('selectedDivision');
-    const savedSelectedGroup = localStorage.getItem('selectedGroup');
-    const savedSelectedClasses = localStorage.getItem('selectedClasses');
-    const savedSelectedSubclass = localStorage.getItem('selectedSubclass');
-    const savedSelectedActivities = localStorage.getItem('selectedActivities');
-
-    if (savedSelectedSection) setSelectedSection(JSON.parse(savedSelectedSection));
-    if (savedSelectedDivision) setSelectedDivision(JSON.parse(savedSelectedDivision));
-    if (savedSelectedGroup) setSelectedGroup(JSON.parse(savedSelectedGroup));
-    if (savedSelectedClasses) setSelectedClasses(JSON.parse(savedSelectedClasses));
-    if (savedSelectedSubclass) setSelectedSubclass(JSON.parse(savedSelectedSubclass));
-    if (savedSelectedActivities) setSelectedActivities(JSON.parse(savedSelectedActivities));
   }, []);
 
-  const saveToLocalStorage = () => {
+  useEffect(() => {
     localStorage.setItem('selectedSection', JSON.stringify(selectedSection));
     localStorage.setItem('selectedDivision', JSON.stringify(selectedDivision));
     localStorage.setItem('selectedGroup', JSON.stringify(selectedGroup));
-    localStorage.setItem('selectedClasses', JSON.stringify(selectedClasses));
+    localStorage.setItem('selectedClass', JSON.stringify(selectedClass));
     localStorage.setItem('selectedSubclass', JSON.stringify(selectedSubclass));
-    localStorage.setItem('selectedActivities', JSON.stringify(selectedActivities));
-  };
+  }, [selectedSection, selectedDivision, selectedGroup, selectedClass, selectedSubclass]);
 
   const handleSectionChange = (selectedOption) => {
     setSelectedSection(selectedOption);
     setSelectedDivision(null);
     setSelectedGroup(null);
-    setSelectedClasses([]);
+    setSelectedClass(null);
     setSelectedSubclass(null);
-    updateSelectedActivities(selectedOption);
-    saveToLocalStorage();
   };
 
   const handleDivisionChange = (selectedOption) => {
     setSelectedDivision(selectedOption);
     setSelectedGroup(null);
-    setSelectedClasses([]);
+    setSelectedClass(null);
     setSelectedSubclass(null);
-    updateSelectedActivities(selectedOption);
-    saveToLocalStorage();
   };
 
   const handleGroupChange = (selectedOption) => {
     setSelectedGroup(selectedOption);
-    setSelectedClasses([]);
+    setSelectedClass(null);
     setSelectedSubclass(null);
-    updateSelectedActivities(selectedOption);
-    saveToLocalStorage();
   };
 
-  const handleClassChange = (selectedOptions) => {
-    setSelectedClasses(selectedOptions);
+  const handleClassChange = (selectedOption) => {
+    setSelectedClass(selectedOption);
     setSelectedSubclass(null);
-    updateSelectedActivities(selectedOptions);
-    saveToLocalStorage();
   };
 
   const handleSubclassChange = (selectedOption) => {
     setSelectedSubclass(selectedOption);
-    updateSelectedActivities(selectedOption);
-    saveToLocalStorage();
   };
 
-  const updateSelectedActivities = (option) => {
-    if (Array.isArray(option)) {
-      const uniqueOptions = option.filter(
-        (opt) => !selectedActivities.some(activity => activity.value === opt.value)
-      );
-      setSelectedActivities([...selectedActivities, ...uniqueOptions]);
-    } else {
-      const alreadySelected = selectedActivities.some(activity => activity.value === option.value);
-      if (!alreadySelected) {
-        setSelectedActivities([...selectedActivities, option]);
-      }
-    }
-    saveToLocalStorage();
-  };
-
-  const handleDeleteActivity = (activityToDelete) => {
-    setSelectedActivities(selectedActivities.filter(activity => activity.value !== activityToDelete.value));
-    saveToLocalStorage();
+  const handleDeleteActivity = () => {
+    setSelectedSection(null);
+    setSelectedDivision(null);
+    setSelectedGroup(null);
+    setSelectedClass(null);
+    setSelectedSubclass(null);
   };
 
   const handleNext = () => {
-    if (!selectedSection || !selectedDivision || !selectedGroup || selectedClasses.length === 0 || !selectedSubclass) {
+    if (!selectedSection || !selectedDivision || !selectedGroup || !selectedClass || !selectedSubclass) {
       setAlertMessage('Veuillez sélectionner toutes les options avant de continuer.');
       setOpenSnackbar(true);
     } else {
@@ -132,7 +100,7 @@ const SelectionActivite = () => {
     setOpenSnackbar(false);
   };
 
-  if (loading) {
+  if (loading && firstVisit) {
     return (
       <Container>
         <Typography variant="h4" gutterBottom style={{ color: '#286AC7' }}>
@@ -156,6 +124,8 @@ const SelectionActivite = () => {
     );
   }
 
+  const subclassesOptions = data?.subclasses[selectedClass?.value] || [];
+
   return (
     <Container>
       <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom={2}>
@@ -166,8 +136,8 @@ const SelectionActivite = () => {
           <Button
             variant="outlined"
             onClick={handleBack}
-            startIcon={<ArrowBackIcon />}
-            style={{ marginRight: 10 }}
+            startIcon={<NavigateBeforeIcon />}
+            style={{ marginRight: 10, borderRadius: '8px', textTransform: 'none' }}
           >
             Retour
           </Button>
@@ -176,30 +146,26 @@ const SelectionActivite = () => {
             color="primary"
             onClick={handleNext}
             endIcon={<NavigateNextIcon />}
+            style={{ borderRadius: '8px', textTransform: 'none' }}
           >
             Suivant
           </Button>
         </Box>
       </Box>
-      {selectedActivities.length > 0 && (
-        <Box marginTop={2}>
-          <Typography variant="h6">
-            Vous avez choisi les activités:
-          </Typography>
-          <Box display="flex" flexWrap="wrap" gap={1}>
-            {selectedActivities.map(activity => (
-              <Chip
-                key={activity.value}
-                label={activity.label}
-                style={{ backgroundColor: 'white', color: 'blue', borderColor: 'blue' }}
-                onDelete={() => handleDeleteActivity(activity)}
-                deleteIcon={<span style={{ color: 'grey' }}>×</span>}
-                variant="outlined"
-              />
-            ))}
+
+      {selectedSubclass && (
+        <Paper elevation={0} style={{ padding: '4px 8px', marginBottom: '16px', width: 'fit-content', border: '1px solid #ccc' }}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="caption" style={{ marginRight: '8px' }}>
+              <span style={{ color: '#286AC7' }}>Vous avez choisi l'activité</span> : {selectedSubclass.label}
+            </Typography>
+            <IconButton onClick={handleDeleteActivity} size="small" style={{ color: 'grey' }}>
+              <CloseIcon fontSize="small" style={{ fontSize: '16px' }} />
+            </IconButton>
           </Box>
-        </Box>
+        </Paper>
       )}
+
       <Snackbar
         open={openSnackbar}
         autoHideDuration={5000}
@@ -292,11 +258,10 @@ const SelectionActivite = () => {
               </Typography>
               <FormControl fullWidth>
                 <Select
-                  value={selectedClasses}
+                  value={selectedClass}
                   onChange={handleClassChange}
                   options={data.classes[selectedGroup.value] || []}
                   placeholder="Sélectionnez une classe"
-                  isMulti
                   isSearchable
                   styles={{
                     control: (base) => ({
@@ -309,7 +274,7 @@ const SelectionActivite = () => {
             </Grid>
           )}
 
-          {selectedClasses.length > 0 && (
+          {selectedClass && (
             <Grid item xs={12}>
               <Typography variant="h6" gutterBottom style={{ color: '#286AC7' }}>
                 Sous-classe
@@ -318,7 +283,7 @@ const SelectionActivite = () => {
                 <Select
                   value={selectedSubclass}
                   onChange={handleSubclassChange}
-                  options={data.subclasses[selectedClasses.map(cls => cls.value)] || []}
+                  options={subclassesOptions}
                   placeholder="Sélectionnez une sous-classe"
                   isSearchable
                   styles={{
