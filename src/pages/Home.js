3a67ca1a30487298395bdbebee885a1d7ac413f3
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-06-09 18:21:43 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 import { Container, Box, Grid, Card, CardContent, Typography, Button, Paper } from '@mui/material';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import SummarizeIcon from '@mui/icons-material/Summarize';
+
+// Chemins des icônes dans le dossier public
+const BusinessCenterIcon = '/machine-vision-svgrepo-com.svg';
+const LocationOnIcon = '/intelligent-positioning-svgrepo-com.svg';
+const SummarizeIcon = '/data-analysis-svgrepo-com.svg';
 
 const steps = [
-  { id: 1, title: "Sélection d'une ou plusieurs activités associées: utilisez le menu ", strongText: "Sélection de l'activité", icon: <BusinessCenterIcon fontSize="large" /> },
-  { id: 2, title: "Sélection géographique autour de votre zone d'implantation: utilisez le menu ", strongText: "Localisation d'implantation", icon: <LocationOnIcon fontSize="large" /> },
-  { id: 3, title: "Édition d'un dossier statistique: utilisez le menu ", strongText: "Synthèse de la recherche", icon: <SummarizeIcon fontSize="large" /> }
+  { id: 1, title: "Sélection d'une ou plusieurs activités associées: utilisez le menu ", strongText: "Sélection de l'activité", icon: <img src={BusinessCenterIcon} alt="Business Center Icon" style={{ width: '60px', height: '60px' }} /> },
+  { id: 2, title: "Sélection géographique autour de votre zone d'implantation: utilisez le menu ", strongText: "Localisation d'implantation", icon: <img src={LocationOnIcon} alt="Location On Icon" style={{ width: '60px', height: '60px' }} /> },
+  { id: 3, title: "Édition d'un dossier statistique: utilisez le menu ", strongText: "Synthèse de la recherche", icon: <img src={SummarizeIcon} alt="Summarize Icon" style={{ width: '60px', height: '60px' }} /> }
 ];
 
 const Home = () => {
-  const navigate = useNavigate(); // Initialize useNavigate
+  const navigate = useNavigate();
 
   const handleStartStudy = () => {
-    navigate('/selection-activite'); // Redirect to the activity selection page
+    navigate('/selection-activite');
   };
 
   return (
     <Container sx={{ mt: 5 }}>
-      <Typography variant="h4" component="h1" gutterBottom sx={{ textAlign: 'center', color:'#e4003a' }}>
+      <Typography variant="h4" component="h1" gutterBottom sx={{ textAlign: 'center', color: '#e4003a' }}>
         Mon entreprise avec les données de l'Insee
       </Typography>
       <br />
