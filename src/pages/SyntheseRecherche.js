fa1fad17d3bf1674e62f1da746054299feb3237c
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-06-14 07:55:39 +0200

    Update SyntheseRecherche.js

--- src/pages/SyntheseRecherche.js
+++ src/pages/SyntheseRecherche.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { Container, Paper, Typography, Box, Tabs, Tab, IconButton, Tooltip, Divider, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Paper, Typography, Box, Tabs, Tab, Button, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import PopulationTab from './PopulationTab';
 import EntreprisesTab from './EntreprisesTab';
-import GetAppIcon from '@mui/icons-material/GetApp';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import * as XLSX from 'xlsx';
-import html2canvas from 'html2canvas';
 
 const SyntheseRecherche = () => {
   const [selectedNAF, setSelectedNAF] = useState('');
   const [selectedNAF1, setSelectedNAF1] = useState('');
   const [selectedCommunes, setSelectedCommunes] = useState([]);
+  const [selectedCommuneNames, setSelectedCommuneNames] = useState([]);
   const [tabIndex, setTabIndex] = useState(0);
-  const populationRef = useRef();
-  const entreprisesRef = useRef();
+
   const navigate = useNavigate();
 
   useEffect(() => {
     const naf = localStorage.getItem('selectedNAF');
     const communes = JSON.parse(localStorage.getItem('selectedCommunes')) || [];
+    const communeNames = JSON.parse(localStorage.getItem('selectedCommuneNames')) || [];
+    console.log('Loaded communes from localStorage:', communes);
+    console.log('Loaded commune names from localStorage:', communeNames);
     setSelectedNAF(naf || '');
     setSelectedCommunes(communes || []);
+    setSelectedCommuneNames(communeNames || []);
   }, []);
 
   useEffect(() => {
@@ -34,45 +35,17 @@ const SyntheseRecherche = () => {
     setTabIndex(newValue);
   };
 
-  const handleDownload = async () => {
-    const populationData = populationRef.current ? populationRef.current.getPopulationData() : [];
-    const entreprisesData = entreprisesRef.current ? entreprisesRef.current.getEntreprisesData() : [];
-
-    console.log("Population Data for export:", populationData);
-    console.log("Entreprises Data for export:", entreprisesData);
-
-    const workbook = XLSX.utils.book_new();
-    const populationSheet = XLSX.utils.json_to_sheet(populationData);
-    XLSX.utils.book_append_sheet(workbook, populationSheet, "Population");
-    const entreprisesSheet = XLSX.utils.json_to_sheet(entreprisesData);
-    XLSX.utils.book_append_sheet(workbook, entreprisesSheet, "Entreprises");
-
-    XLSX.writeFile(workbook, "Medl'In.xlsx");
-
-    if (tabIndex === 0 && populationRef.current) {
-      const populationElement = populationRef.current.getTableElement();
-      if (populationElement) {
-        const canvas = await html2canvas(populationElement);
-        const link = document.createElement('a');
-        link.href = canvas.toDataURL('image/png');
-        link.download = 'PopulationTab.png';
-        link.click();
-      }
-    }
-  };
-
   const handleBack = () => {
-    navigate('/localisation-implantation'); // Adjust this path as needed
+    navigate('/localisation-implantation');
   };
 
   const handleNext = () => {
-    // Implement navigation to the next page if needed
-    navigate('/Aide'); // Adjust this path as needed
+    navigate('/Aide');
   };
 
   return (
     <Container>
-      <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom={2} >
+      <Box display="flex" justifyContent="space-between" alignItems="center" marginBottom={2}>
         <Typography variant="h4" gutterBottom>Synthèse de la recherche</Typography>
         <Box display="flex" alignItems="center">
           <Button
@@ -97,15 +70,14 @@ const SyntheseRecherche = () => {
       <Typography variant="body1" paragraph style={{ marginTop: '20px' }}>
         Veuillez trouver ci-dessous les résultats issus de votre étude.
       </Typography>
-      <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+      <Paper elevation={1} style={{ padding: '16px', marginBottom: '16px' }}>
         <Typography variant="h6" gutterBottom>Activité choisie :</Typography>
         <Typography variant="body1">{selectedNAF1 || 'Aucune activité choisie'}</Typography>
       </Paper>
-      <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+      <Paper elevation={1} style={{ padding: '16px', marginBottom: '16px' }}>
         <Typography variant="h6" gutterBottom>Communes choisies :</Typography>
         <Typography variant="body1">
-          {selectedCommunes.length > 0 ? selectedCommunes.map(commune => commune.value).join(', ') : 'Aucune commune choisie'}
-        </Typography>
+        {selectedCommuneNames.length > 0 ? selectedCommuneNames.join(', ') : 'Aucune commune choisie'}        </Typography>
       </Paper>
 
       <Box marginTop={10} position="relative">
@@ -139,30 +111,16 @@ const SyntheseRecherche = () => {
             }} 
           />
         </Tabs>
-        <Tooltip title="Exporter les données">
-          <IconButton
-            style={{ 
-              position: 'absolute', 
-              top: 0, 
-              right: 0, 
-              border: '1px solid lightgrey',
-            }}
-            onClick={handleDownload}
-          >
-            <GetAppIcon />
-          </IconButton>
-        </Tooltip>
         <Divider style={{ marginTop: '16px' }} />
       </Box>
       {tabIndex === 0 && (
         <div style={{ backgroundColor: 'white', marginTop: '16px', padding: '16px' }}>
-          <PopulationTab ref={populationRef} communeCode={selectedCommunes[0]?.value} />
+          <PopulationTab />
         </div>
       )}
       {tabIndex === 1 && (
         <div style={{ backgroundColor: 'white', marginTop: '16px', padding: '16px' }}>
-        <EntreprisesTab ref={entreprisesRef} selectedNAF={selectedNAF} selectedCommunes={selectedCommunes} />
-
+          <EntreprisesTab selectedNAF={selectedNAF} selectedCommunes={selectedCommunes} />
         </div>
       )}
     </Container>
