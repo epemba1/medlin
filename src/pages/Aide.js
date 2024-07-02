ae84e93e5358ddca6eb557bf3f83ff88ccfc7698
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-07-02 22:32:01 +0200

    Update Aide.js

--- src/pages/Aide.js
+++ src/pages/Aide.js
@@ -4,32 +4,48 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore'; // Import the icon 
 
 const Aide = memo(() => {
   const faqs = [
-    //Questions and Answers
-    { question: "Comment utiliser l'application Medl'In ?", answer: "Pour utiliser l'application, commencez par sélectionner une activité, puis choisissez une localisation et enfin consultez la synthèse de la recherche." },
-    { question: "Comment connaître mon Naf ?", answer: "Vous le trouvez dans votre espace personnel du Guichet des formalités des entreprises dès que votre demande d'immatriculation est acceptée." },
-    { question: "Quel sont les code NAF pour mon activité ?", answer: "Il vous suffit de consulter la nomenclature NAF sur le site de l'INSEE. La recherche par mots-clés vous aidera à déterminer quel code NAF est le plus approprié à l'activité de votre entreprise." },
-    { question: "Comment obtenir un rapport ?", answer: "Une fois les étapes complétées, vous pouvez télécharger un rapport depuis le menu 'Synthèse de la recherche'." }
+    {
+      question: "Comment utiliser l'application Medl'In ?",
+      answer: "Pour utiliser l'application, commencez par sélectionner une activité, puis choisissez une localisation et enfin consultez la synthèse de la recherche."
+    },
+    {
+      question: "Comment connaître mon Naf ?",
+      answer: "Vous le trouvez dans votre espace personnel du Guichet des formalités des entreprises dès que votre demande d'immatriculation est acceptée."
+    },
+    {
+      question: "Quel sont les code NAF pour mon activité ?",
+      answer: (
+        <>
+          Il vous suffit de consulter la nomenclature NAF&nbsp;
+          <a href="https://www.insee.fr/fr/metadonnees/nafr2" target="_blank" rel="noopener noreferrer" style={{ color: '#286AC7' }}>
+            sur le site de l'INSEE
+          </a>. La recherche par mots-clés vous aidera à déterminer quel code NAF est le plus approprié à l'activité de votre entreprise.
+        </>
+      )
+    },
+    {
+      question: "Comment obtenir un rapport ?",
+      answer: "Une fois les étapes complétées, vous pouvez télécharger un rapport depuis le menu 'Synthèse de la recherche'."
+    }
   ];
 
   return (
     <Container sx={{ mt: 2 }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Aide
+      <Typography variant="h3" component="h1" gutterBottom style={{textAlign: 'center', fontWeight: 'bold'}}>
+          Questions fréquentes
       </Typography>
       <br />
-      <Typography variant="h6" component="h1" gutterBottom>
-          Questions les plus fréquentes :
-      </Typography>
+      
       <Box sx={{ mt: 4, mb: 2 }}>
         {faqs.map((faq, index) => (
-          <Accordion key={index} sx={{ mb: 2 }}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}> {/* Add the icon here */}
-              <Typography variant="h6" component="h2">
+          <Accordion key={index} sx={{ mb: 2 }} defaultExpanded>
+            <AccordionSummary expandIcon={<ExpandMoreIcon color='primary' sx={{ fontSize: '1.5rem' }}/>}> {/* Add the icon here */}
+              <Typography variant="h6" component="h2" color="primary" style={{ fontWeight: 'bold'}}>
                 {faq.question}
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography variant="body1" component="p">
+              <Typography variant="body1" component="p" style={{color: 'rgb(96, 96, 96)'}}>
                 {faq.answer}
               </Typography>
             </AccordionDetails>
@@ -41,3 +57,5 @@ const Aide = memo(() => {
 });
 
 export default Aide;
+
+//periode(activitePrincipaleEtablissement:70.22Z AND etatAdministratifEtablissement:A) AND codeCommuneEtablissement:69005
